3984cff5daf5392a79c5b77dbe0deb0e7156bb3d
wanching_97 <wanching_97@hotmail.com>
2018-05-11 02:31:02 +0800

    setup geocode.py

---
+++ geocode.py
@@ -0,0 +1,12 @@
+import httplib2
+import json
+
+def getGeocodeLocation(inputString):
+	google_api_key = ""
+	locationString = inputString.replace(" ", "+")
+	url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'% (locationString, google_api_key))
+
+	h = httplib2.Http()
+	response, content = h.request(url, 'GET')
+	result = json.loads(content)
+	return result
