11670136f166290c0ff11c31d9e8b31b60549b56
wanching_97 <wanching_97@hotmail.com>
2018-05-11 18:30:42 +0800

    added API key

--- geocode.py
+++ geocode.py
@@ -2,7 +2,7 @@ import httplib2
 import json
 
 def getGeocodeLocation(inputString):
-	google_api_key = ""
+	google_api_key = "AIzaSyBEJ6VAb2eZ3_uehWlSeok_0whITsfaPlo"
 	locationString = inputString.replace(" ", "+")
 	url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'% (locationString, google_api_key))
 
