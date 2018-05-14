0b2ae3b24917b36cc81e6f013538e20430304479
wanching_97 <wanching_97@hotmail.com>
2018-05-14 19:30:40 +0800

    done tester for get request in tester3

--- endpointTester3.py
+++ endpointTester3.py
@@ -24,3 +24,20 @@ except Exception as err:
 else: 
 	print("test 1 pass: successfully made post request to /puppies")
 
+
+# making a get request
+print("making get request for /puppies...")
+try:
+	url = address + "/puppies"
+	h = httplib2.Http()
+	response, result = h.request(url, 'GET')
+	if response['status'] != '200':
+		raise Exception('Received an unsuccessful status code of %s' % resp['status'])
+except Exception as err:
+	print "Test 2 FAILED: Could not make GET Request to web server"
+	print err.args
+	sys.exit()
+else:
+	print "Test 2 PASS: Succesfully Made GET Request to /puppies"
+
+
