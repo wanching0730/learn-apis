7144328f710aaa95f70dc4b23582b86e2434ea0e
wanching_97 <wanching_97@hotmail.com>
2018-05-14 19:23:38 +0800

    edited syntax for endpoint2 when testing

--- endpoint2.py
+++ endpoint2.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -34,6 +34,6 @@ def updatePuppy(id):
 def deletePuppy(id):
   return "Removing Puppy with id %s" % id
 
- if __name__ == '__main__':
- 	app.debug = True
- 	app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+	app.debug = True
+	app.run(host='0.0.0.0', port=5000)
